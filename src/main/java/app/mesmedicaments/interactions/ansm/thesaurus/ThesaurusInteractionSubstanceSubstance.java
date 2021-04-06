6f2e365abfde400206ac16cf945a799111270ffe
Axel Ogereau-Peltier <49279289+axel-op@users.noreply.github.com>
2021-04-06 12:40:50 +0200

    Conserve ordre des substances dans l'interaction

--- src/main/java/app/mesmedicaments/interactions/ansm/thesaurus/ThesaurusInteractionSubstanceSubstance.java
+++ src/main/java/app/mesmedicaments/interactions/ansm/thesaurus/ThesaurusInteractionSubstanceSubstance.java
@@ -1,12 +1,20 @@
 package app.mesmedicaments.interactions.ansm.thesaurus;
 
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 public final class ThesaurusInteractionSubstanceSubstance
         extends ThesaurusInteraction<ThesaurusSubstance, ThesaurusSubstance> {
+
+    static private Comparator<ThesaurusSubstance> comparator =
+            Comparator.comparing(ThesaurusSubstance::getName);
 
     public ThesaurusInteractionSubstanceSubstance(ThesaurusSubstance substance1,
             ThesaurusSubstance substance2, ThesaurusInteractionLevel level, String description,
             String conduiteATenir) {
-        super(substance1, substance2, level, description, conduiteATenir);
+        super(Stream.of(substance1, substance2).min(comparator).get(),
+                Stream.of(substance1, substance2).max(comparator).get(), level, description,
+                conduiteATenir);
     }
 
 }
