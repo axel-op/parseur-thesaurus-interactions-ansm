766feb62d7b9448a1d7478a3aebeeff5ece04a01
Axel Ogereau-Peltier <49279289+axel-op@users.noreply.github.com>
2021-04-06 12:47:36 +0200

    Conserve ordre des classes dans l'interaction

--- src/main/java/app/mesmedicaments/interactions/ansm/thesaurus/ThesaurusInteractionClasseClasse.java
+++ src/main/java/app/mesmedicaments/interactions/ansm/thesaurus/ThesaurusInteractionClasseClasse.java
@@ -1,12 +1,19 @@
 package app.mesmedicaments.interactions.ansm.thesaurus;
 
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 public final class ThesaurusInteractionClasseClasse
         extends ThesaurusInteraction<ThesaurusClasse, ThesaurusClasse> {
 
-    public ThesaurusInteractionClasseClasse(ThesaurusClasse classe1,
-            ThesaurusClasse classe2, ThesaurusInteractionLevel level, String description,
-            String conduiteATenir) {
-        super(classe1, classe2, level, description, conduiteATenir);
+    static private Comparator<ThesaurusClasse> comparator =
+            Comparator.comparing(ThesaurusClasse::getName);
+
+    public ThesaurusInteractionClasseClasse(ThesaurusClasse classe1, ThesaurusClasse classe2,
+            ThesaurusInteractionLevel level, String description, String conduiteATenir) {
+        super(Stream.of(classe1, classe2).min(comparator).get(),
+                Stream.of(classe1, classe2).max(comparator).get(), level, description,
+                conduiteATenir);
     }
 
 }
